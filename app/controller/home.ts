7b0b1047705231bff594fade362ff0fe3296f9b2
rainwalker <kinghu1990@sina.com>
2019-10-22 14:35:35 +0800

    chore: add psd.js to parse .psd file

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -1,8 +1,14 @@
 import { Controller } from 'egg';
+const PSD = require('psd');
+import { resolve } from 'path';
 
 export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
-    ctx.body = await ctx.service.test.sayHi('egg');
+    const file = resolve(__dirname, './example.psd');
+    const psd = PSD.fromFile(file);
+    psd.parse();
+    const tree = psd.tree().export();
+    ctx.body = tree;
   }
 }
